6c7e90e31bd07497a2d32b5856f761db04e6b201
Eoin <eoinlynaghgithub@gmail.com>
2020-06-17 12:58:02 -0400

    updated to not be terrible
    using the csv library now

--- src/CSV-To-Scrobbles.py
+++ src/CSV-To-Scrobbles.py
@@ -1,42 +1,45 @@
+import csv
+
 new=open("new.csv","w")
+log=open("log.txt","w")
+writer = csv.writer(new)
+
 songs = []
 c = 0
 num = 1
+
+failedSongs = ["FAILED SONGS: \n"]
+songsMissingPlayValues = ["SONGS MISSING DATA IN SPREADSHEET: \n"]
+
 with open("old.csv","r") as f:
-    for line in f:
-        songConstructor = []
-        #if your song/artist/whatever has more than one comma you are out of luck, that should be unusual though
-        lineSplitQuotes=line.split("\"")
-        if(len(lineSplitQuotes) == 3):
-            for value in lineSplitQuotes:
-                if(value != lineSplitQuotes[1]): #split around comma
-                    for x in value.split(","):
-                        if(x):
-                            songConstructor.append(x)
-                else:
-                    songConstructor.append("\""+value+"\"")
-            songs.append(songConstructor)
-        elif(len(lineSplitQuotes) == 1):
-            vals=line.split(",")
-            for value in vals:
-                if(value):
-                    songConstructor.append(value)
-            songs.append(songConstructor)
-    for song in songs:
+    reader = csv.reader(f)
+    for song in reader:
         try:
+            if(song[4] == ''):
+                song[4] = '0'
+                if(songsMissingPlayValues.count("artist: " + song[0] + ", song name: " + song[2] + "\n") <= 0):
+                    songsMissingPlayValues.append("artist: " + song[0] + ", song name: " + song[2] + "\n")
             for i in range(0,int(song[4])+1):
                     c += 1
                     if(c == 2750):
                         c = 0
                         name = "new"+str(num)+".csv"
                         new=open(name,"w")
+                        writer = csv.writer(new)
                         num += 1
                     if(len(song) == 7):
-                        new.write(song[0]+","+song[1]+","+song[2]+","+song[3]+","+song[5]+","+song[6])
+                        songWriteValue = song[:]
+                        del songWriteValue[4]
+                        writer.writerow(songWriteValue)
                     else:
                         raise("song did not parse properly")
         except:
-            print("song failed:")
-            print("\t artist: " + song[0])
-            print("\t   name: " + song[1] + "\n")
+            if(failedSongs.count("artist: " + song[0] + ", song name: " + song[2] + "\n") <= 0):
+                failedSongs.append("artist: " + song[0] + ", song name: " + song[2] + "\n")
             continue
+
+print(str(len(failedSongs)-1) + " song(s) have failed to be added, please check log.txt for more information")
+log.writelines(failedSongs)     
+print(str(len(songsMissingPlayValues)-1) + " song(s) were missing data on the number of plays, please check log.txt for more information")
+print("They have been assumed to have only been played once.")
+log.writelines(songsMissingPlayValues)     
